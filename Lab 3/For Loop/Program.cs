cc6cef52c2e80b8a7a9378b2d198997f48d5afc7
Choksi99 <62966265+Choksi99@users.noreply.github.com>
2020-06-22 14:24:50 +1200

    Add files via upload

---
+++ Lab 3/For Loop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int i_StartYear = 2018;
+            for (int i_CurrentYear = i_StartYear; i_CurrentYear <= i_StartYear + 10; i_CurrentYear++)
+            {
+                Console.Write(i_CurrentYear + " ");
+            }
+
+        }
+    }
+}
